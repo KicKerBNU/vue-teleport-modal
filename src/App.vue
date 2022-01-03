f9a838ea8196029440b283d498be81759195dafa
Everton Alcides Buzzi <evertonbuzzi@gmail.com>
2022-01-03 16:46:01 -0300

    EB: Teleport Modals to body tag

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="pug">
+div.container.center-align
+  h5 {{title}}
+  p Welcome
+  teleport(to=".modals" v-if="showModal")
+    Modal(theme="" @close="toggleModal")
+      template(v-slot:links)
+        a(href="#") Sign Up
+        a(href="#") More Info
+      h1 Lorem Ipsum 
+      p More lorem ipsum
+  teleport(to=".modals" v-if="showModalTwo")
+    Modal(@close="toggleModalTwo")
+      h1 newsletter 
+      p get updates
+    
+  button.small(@click="toggleModal") open modal (alt)
+  button.small(@click="toggleModalTwo") open modal
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal.vue";
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Modal },
+  data() {
+    return {
+      title: 'Example using TELEPORT',
+      showModal: false,
+      showModalTwo: false
+      
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo(){
+      this.showModalTwo = !this.showModalTwo;
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+div.backdrop(@click.self="closeModal")
+    .modal(:class="{ sale: theme === 'sale' }")
+        slot default content
+        div.actions
+            slot(name="links")
+
+</template>
+
+<script>
+export default {
+    props: ['theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+</style>
